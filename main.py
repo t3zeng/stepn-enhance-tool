f842097828ae7efc7ff8b0f62623d2d55f4e6b83
Tian Zeng <t3zeng@gmail.com>
2023-03-26 15:58:25 -0400

    output chance of top x results

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-def calculate_slot_probabilities(items):
+def calculate_slot_probabilities(items, top_n=10):
+    if len(items) != 5:
+        print("Must use 5 items for enhancement!")
+        return -1
+
     slot_probabilities = []
     for i in range(4):
         slot_types = {"E": 0, "C": 0, "L": 0, "R": 0}
         for item in items:
-            slot_type = item[i]
+            slot_type = item["slot" + str(i+1)]
             if slot_type in slot_types:
                 slot_types[slot_type] += 1
         slot_probability = {}
@@ -11,36 +15,35 @@ def calculate_slot_probabilities(items):
             probability = count / len(items)
             slot_probability[slot_type] = probability
         slot_probabilities.append(slot_probability)
+
     results = {}
-    for i in range(len(slot_probabilities)):
-        for j in range(i+1, len(slot_probabilities)):
-            for k in range(j+1, len(slot_probabilities)):
-                for l in range(k+1, len(slot_probabilities)):
+    for i in range(len(slot_probabilities[0])):
+        for j in range(len(slot_probabilities[1])):
+            for k in range(len(slot_probabilities[2])):
+                for l in range(len(slot_probabilities[3])):
                     result = ""
-                    for slot_type in ["E", "C", "L", "R"]:
-                        max_probability = max(slot_probabilities[i][slot_type], slot_probabilities[j][slot_type], slot_probabilities[k][slot_type], slot_probabilities[l][slot_type])
-                        if max_probability == 0:
-                            result += "-"
-                        else:
-                            for slot in [i, j, k, l]:
-                                if slot_probabilities[slot][slot_type] == max_probability:
-                                    result += items[slot][slot_type]
-                                    break
+                    total_probability = 1
+                    for m, slot in enumerate([i, j, k, l]):
+                        slot_key = "slot" + str(m+1)
+                        slot_type = list(slot_probabilities[m].keys())[slot]
+                        result += slot_key + slot_type
+                        total_probability *= slot_probabilities[m][slot_type]
                     if result in results:
-                        results[result] += 1
+                        results[result] += total_probability
                     else:
-                        results[result] = 1
-    total_combinations = sum(results.values())
-    for result, count in sorted(results.items(), key=lambda x: x[1], reverse=True):
-        probability = count / total_combinations
+                        results[result] = total_probability
+
+    sorted_results = sorted(results.items(), key=lambda x: x[1], reverse=True)
+    for i in range(min(top_n, len(sorted_results))):
+        result, probability = sorted_results[i]
         print(result + ":", probability)
 
 # Example usage:
 items = [
-    {"E": "L", "E": "C"},
-    {"E": "L", "C": "C"},
-    {"C": "L", "L": "E"},
-    {"E": "E", "C": "C"},
-    {"L": "C", "L": "L"}
+    {"slot1": "E", "slot2": "L", "slot3": "E", "slot4": "C"},
+    {"slot1": "E", "slot2": "L", "slot3": "C", "slot4": "C"},
+    {"slot1": "C", "slot2": "L", "slot3": "L", "slot4": "E"},
+    {"slot1": "E", "slot2": "E", "slot3": "C", "slot4": "C"},
+    {"slot1": "L", "slot2": "C", "slot3": "L", "slot4": "L"}
 ]
-calculate_slot_probabilities(items)
+calculate_slot_probabilities(items, top_n=20)
